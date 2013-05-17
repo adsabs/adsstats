3e72592e6f7a0d48d8f7e85e516a8aaf6658843b
Edwin Henneken <ehenneken@gmail.com>
2013-05-17 16:15:01 -0400

    config: added basic MongoDB configs

--- config/config.py
+++ config/config.py
@@ -5,8 +5,12 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 class AppConfig(object):
 
 #    LOG_DIR = os.path.exists(_basedir + "/logs") and _basedir + "/logs" or "."
+    MONGO_DATABASE = 'adsdata'
     MONGO_HOST = "localhost"
+    MONGO_PORT = 27017
     MONGO_SAFE = True
+    MONGO_USER = 'adsdata'
+    MONGO_PASSWORD = ''
 
     SOLR_URL = 'http://adswhy:9000/solr/collection1/select'
 try:
