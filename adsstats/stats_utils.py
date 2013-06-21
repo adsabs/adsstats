95019d994421827e18a1413ca881e20b224cf37d
Edwin Henneken <ehenneken@gmail.com>
2013-06-21 13:40:09 -0400

    stats_utils: does it actually work?

--- adsstats/stats_utils.py
+++ adsstats/stats_utils.py
@@ -279,8 +279,8 @@ def get_attributes(args):
     result=Pool(threads).map(get_citation_dictionary,bibcodes)
     duration = time.time() - stime
     print "  duration: %s sec" % duration
-    print [item for sublist in cit_dict.values() for item in sublist]
-#    print map(lambda b: b[1]*b[2],filter(lambda a: len(a) > 0, cit_dict.values()))
+    tori_list = [item for sublist in cit_dict.values() for item in sublist]
+    print map(lambda b: b[1]*b[2],filter(lambda a: len(a) > 0, tori_list))
     Nciting = len(utils.flatten(cit_dict.values()))/2
     Nciting_ref = len(utils.flatten(ref_cit_dict.values()))/2
     print "  total: %s citations (%s refereed citations)" % (Nciting, Nciting_ref)
