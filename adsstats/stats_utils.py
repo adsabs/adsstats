475d889bde6e4b058d023d90e96230369973cc36
Edwin Henneken <ehenneken@cfa.harvard.edu>
2013-07-23 14:14:20 -0400

    debugging...

--- adsstats/stats_utils.py
+++ adsstats/stats_utils.py
@@ -265,6 +265,5 @@ def generate(**args):
 
     rez=Pool(config.THREADS).map(generate_data, stats_models)
     print glob_data
-    results = ''
-#    results = format_results()
+    results = format_results()
     return results
